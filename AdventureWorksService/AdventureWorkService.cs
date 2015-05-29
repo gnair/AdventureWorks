361e7640b6a054eb25ba15e0550d2ccea92616fe
Girish Nair <gnair@coverity.com>
2015-05-29 16:30:33 -0500

    checkpoint

---
+++ AdventureWorksService/AdventureWorkService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorksService
+{
+    public partial class AdventureWorkService : ServiceBase
+    {
+        private CustomerServiceHost customerServiceHost = null;
+
+        public AdventureWorkService()
+        {
+            InitializeComponent();
+
+            this.ServiceName = "AdventureWorks Service";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            customerServiceHost = new CustomerServiceHost(typeof(CustomerService));
+            customerServiceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            customerServiceHost.Close();
+        }
+    }
+}
